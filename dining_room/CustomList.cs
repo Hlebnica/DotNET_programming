f4ec45e44d4d648fa5e890e0e593380f7395036a
Дмитрий Старостенок <49001181+Hlebnica@users.noreply.github.com>
2022-10-18 12:51:09 +0500

    Task2_update

--- dining_room/CustomList.cs
+++ dining_room/CustomList.cs
@@ -4,22 +4,65 @@ using System.Collections.Generic;
 
 namespace dining_room
 {
-    public class CustomList : IEnumerable
+    public class CustomList<T> : ICollection<T> where T : Product
     {
-        public List<Object> _products = new();
-
-        public void AppendInList(Object nameOfProduct)
+        
+        public List<T> MyList { get; set; }
+        public int Count => MyList.Count;
+        public bool IsReadOnly => false;
+        public CustomList()
         {
-            _products.Add(nameOfProduct);
+            MyList = new List<T>();
+        }
+        
+        public T this[int index]
+        {
+            get => MyList[index];
+            set => MyList[index] = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            if (!Contains(item))
+            {
+                MyList.Add(item);
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
         }
 
         
-        
-        public void ClearList()
-        {
-            _products.Clear();
-        }
-        
-        public IEnumerator GetEnumerator() => _products.GetEnumerator();
     }
 }
